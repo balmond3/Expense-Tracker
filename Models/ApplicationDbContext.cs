2406d0ee75ee15c49db32d38ab8c69e17c59a5f0
Carlos D Montilla <94148308+balmond3@users.noreply.github.com>
2023-12-04 16:24:51 -0700

    Add project files.

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Expense_Tracker.Models;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options) { 
+        
+
+        }
+       
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Transaction> Categories { get; set; }
+        public DbSet<Expense_Tracker.Models.Category>? Category { get; set; }
+
+
+    }
+
+       
+}
